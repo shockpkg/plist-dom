6a02d8d67071ca6b754b981a3e85d5437d97c0a0
JrMasterModelBuilder <legojrmastermodelbuilder@gmail.com>
2020-04-19 23:43:22 -0400

    Removed some dead code

--- src/values/index.ts
+++ src/values/index.ts
@@ -6,30 +6,3 @@ export * from './dict';
 export * from './integer';
 export * from './real';
 export * from './string';
-
-// import {Value} from '../value';
-//
-// import {ValueArray} from './array';
-// import {ValueBoolean} from './boolean';
-// import {ValueData} from './data';
-// import {ValueDate} from './date';
-// import {ValueDict} from './dict';
-// import {ValueInteger} from './integer';
-// import {ValueReal} from './real';
-// import {ValueString} from './string';
-//
-// export const valueConstructors = [
-// 	ValueArray,
-// 	ValueBoolean,
-// 	ValueData,
-// 	ValueDate,
-// 	ValueDict,
-// 	ValueInteger,
-// 	ValueReal,
-// 	ValueString
-// ];
-//
-// export const valueConstructorsTypes = new Map<string, new() => Value>();
-// for (const Value of valueConstructors) {
-// 	valueConstructorsTypes.set(Value.TYPE, Value);
-// }
